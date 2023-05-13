15124d812c4e92c5c96fd952714573e528f848a1
ChickenIsVeryTasty <63068273+ChickenIsVeryTasty@users.noreply.github.com>
2023-05-13 14:08:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
+var port = process.env.PORT || 8080;
+ 
+var cors_proxy = require('cors-anywhere');
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins 
+    requireHeader: [],
+    removeHeaders: [],
+    setHeaders: {"referer": "https://revisionvillage.com"}
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
